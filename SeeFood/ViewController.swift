da573daae817f4f5ee9dd6a45719815b754ac279
Matthew Tripodi <mtripodi1027@gmail.com>
2019-04-14 18:19:05 -0400

    Hotdog not Hotdog functionality added

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -50,6 +50,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             
             print(results)
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+            
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
